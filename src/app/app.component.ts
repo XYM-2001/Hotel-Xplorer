43e70dbdf05cc4978396d223800214c3643d434e
Xinyu Meng <xmeng975@gmail.com>
2024-08-05 04:09:04 -0500

    updated module components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome'
+import { FontAwesomeIcons } from './shared/font-awesome-icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule, ButtonModule],
+  imports: [RouterOutlet,CommonModule, ButtonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'hotelxplorer-front';
+export class AppComponent implements OnInit{
+
+  faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome(){
+    this.faIconLibrary.addIcons(...FontAwesomeIcons);
+  }
+
 }
