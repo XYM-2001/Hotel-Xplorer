5f7c2646647bd42b885e64d596173d67ead10a7b
Xinyu Meng <xmeng975@gmail.com>
2024-08-06 17:39:56 -0500

    updated category

---
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.scss'
+})
+export class AvatarComponent {
+
+    imageUrl = input<string>();
+    avatarSize = input<"avatar-sm" | "avatar-xl">();
+}
